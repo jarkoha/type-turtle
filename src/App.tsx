50b8e96d1bead2a7e491abb567e7e69a0b6fffe5
Jarl Koha <146342562+jarkoha@users.noreply.github.com>
2024-09-16 17:31:05 +0300

    feat: React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import MyContainer from "./layout/MyContainer";
 import LoginForm from "./components/LoginForm";
-
+import { Outlet } from "react-router-dom";
+//vaata LoginFormi osa üle, sest MyContainer kaob ära
 function App() {
   return (
-    <MyContainer>
-      <LoginForm />
-    </MyContainer>
+    <>
+      <h1>Header</h1>
+      <Outlet />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<App />}
+    >
+      <Route
+        path="extra"
+        element={<h1>Extra</h1>}
+      />
+      <Route
+        path="loginform"
+        element={<h1>MyForm</h1>} //vaata üle, mis siia läheb
+      />
+      <Route
+        path="contact"
+        element={<h1>Contact</h1>}
+      />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
